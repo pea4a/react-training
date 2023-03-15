d7d905a4890414c10a2c37c04bcd824a1a5ea89b
pea4a <72973504+pea4a@users.noreply.github.com>
2023-03-15 22:52:24 +0200

    (feat) add modal for create post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { red } from '@mui/material/colors';
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
+import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
+import MyModal from './components/UI/MyModal/MyModal';
 import MySelect from './components/UI/select/MySelect';
 import './styles/App.css';
 const App = () => {
@@ -15,28 +17,26 @@ const App = () => {
         { id: 4, title: 'TjavaScript4', body: 'BDescription' },
         { id: 5, title: 'GjavaScript5', body: 'QDescription' },
     ])
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
-
-    function getSortedPosts() {
-        console.log('lol')
-        if (selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    const [filter, setFilter] = useState({ sort: '', query: '' })
+    const [modal, setModal] = useState(false)
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            console.log('lol')
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
         }
 
         return posts
+    }, [filter.sort, posts])
 
-    }
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase()))
+    }, [filter.query, sortedPosts]
+    )
 
-    const sortedPosts = getSortedPosts()
-
-    const sortPosts = (sort) => {
-        setSelectedSort(sort)
-
-    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -45,33 +45,18 @@ const App = () => {
 
     return (
         <div className='App'>
-            <PostForm create={createPost} />
-            <br style={{ margin: '15px 0' }} />
-            <div>
-                <MyInput
-                    value={searchQuery}
-                    onChange={e => setSearchQuery(e.target.value)}
-                    placeholder='search...'
-                />
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue='sort by'
-                    options={[
-                        { value: 'title', name: 'by name' },
-                        { value: 'body', name: 'by desc' },
-                    ]}
-                />
-            </div>
-            {posts.length !== 0
-                ?
-                <PostList remove={removePost} posts={sortedPosts} title='post`s about JS' />
-                :
-                <h1 style={{ textAlign: 'center' }}>
-                    posts not found!
-                </h1>
-            }
-
+            <MyButton onClick={() => setModal(true)}>
+                create post
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} />
+            </MyModal>
+            <hr style={{ margin: '15px 0' }} />
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title='post`s about JS' />
         </div >
     )
 }
---
+++ src/components/PostFilter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MyInput from "./UI/input/MyInput";
+import MySelect from "./UI/select/MySelect";
+
+const PostFilter = ({ filter, setFilter }) => {
+    return (
+
+        <div>
+            <MyInput
+                value={filter.query}
+                onChange={e => setFilter({ ...filter, query: e.target.value })}
+                placeholder='search...'
+            />
+            <MySelect
+                value={filter.sort}
+                onChange={selectedSort => setFilter({ ...filter, sort: selectedSort })}
+                defaultValue='sort by'
+                options={[
+                    { value: 'title', name: 'by name' },
+                    { value: 'body', name: 'by desc' },
+                ]}
+            />
+        </div>
+
+    )
+}
+
+export default PostFilter
